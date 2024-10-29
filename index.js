61bc2c48f591daeb669cfefeaa279971bffba619
Hasna Hena Mow <hasna.hena@welldev.io>
2024-10-29 12:05:32 +0600

    common js module export

---
+++ index.js
@@ -0,0 +1,5 @@
+/** Common JS Example */
+const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+console.log(`29 Celcius to Fahrenheit:`, celciusToFahrenheit(29));
