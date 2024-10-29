889bf45a3f85f8a5d77203768fb20688f645b642
Hasna Hena Mow <hasna.hena@welldev.io>
2024-10-29 12:13:34 +0600

    es module default and named export

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-/** Common JS Example */
-const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+import getPosts, { getPostsLength } from "./postController.js";
 
-console.log(`Random Number: ${generateRandomNumber()}`);
-console.log(`29 Celcius to Fahrenheit:`, celciusToFahrenheit(29));
+console.log(getPosts());
+console.log(`Posts length: ${getPostsLength()}`);
+
+// /** Common JS Example */
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`29 Celcius to Fahrenheit:`, celciusToFahrenheit(29));
---
+++ postController.js
@@ -0,0 +1,10 @@
+const posts = [
+  { id: 1, tittle: "Post One" },
+  { id: 2, tittle: "Post Two" }
+];
+
+const getPosts = () => posts;
+
+export const getPostsLength = () => posts.length;
+
+export default getPosts;
